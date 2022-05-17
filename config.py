1d355bc8783712e539e4ecdd6593f106cff14c30
paullete <paulettenereah3@gmail.com>
2022-05-17 06:12:39 +0300

    updates and changes

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
+
+
 # parent Config class contains configurations that are used in both production and development stages.
 class Config:
     '''
     General configuration parent class
     '''
 
+    QUOTE_API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
 
 
 # ProdConfig subclass contains configurations that are used in production stages of our application and inherits from the parent Config class.
--- manage.py
+++ manage.py
@@ -1,10 +1,16 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from app.models import Comment
+from flask_migrate import Migrate, MigrateCommand
 
+# Creating app instance
 app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
+
+# initialize our Migrate class
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
